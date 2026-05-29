efbe6de842bdb7f56b4ece5b6266f016edbb1934
Supriya <bsupriya112007@gmail.com>
2026-05-29 16:31:26 +0530

    updated

--- api/index.js
+++ api/index.js
@@ -1,23 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
-
-const Note = require("../models/Note");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+let cached = false;
+
+async function connectDB() {
+    if (cached) return;
+
+    await mongoose.connect(process.env.MONGO_URI);
+    cached = true;
+}
+
+app.get("/", (req, res) => {
+    res.send("API Working");
+});
 
 app.get("/notes", async (req, res) => {
+    await connectDB();
+    const Note = require("../models/Note");
     const notes = await Note.find();
     res.json(notes);
 });
 
 app.post("/notes", async (req, res) => {
+    await connectDB();
+    const Note = require("../models/Note");
     const note = await Note.create(req.body);
     res.json(note);
 });
